2eb74de69c4fb05445b3aef75e9717d2d46c91aa
QuanminhN <quanminhn1@gmail.com>
2022-10-29 17:23:29 -0400

    Update LightCandles.cs
    Candles now have a chance of turning off

--- Breakout From Ackers/Assets/Scripts/Items/LightCandles.cs
+++ Breakout From Ackers/Assets/Scripts/Items/LightCandles.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class LightCandles : Interactable
 {
+
     [SerializeField] private GameObject flame;
-    private bool unlit;
+    private bool unlit, canTurnOff = false;
     AudioSource light;
+    float time;
+    [SerializeField] float timeToOff = 100; // seconds to turn off candle
+    [SerializeField] int randMax = 100; // Max random number for random generator
+    [SerializeField] int threshold = 50;// threshold to turn off candle
+    FirstPersonController FPC; //first person controller
+    int score; //The player's score
+    float DD; //Dynamic Difficulty value
     // Start is called before the first frame update
     void Start()
     {
         flame.SetActive(false);
         unlit = true;
         light = gameObject.GetComponentInChildren<AudioSource>();
+        FPC = GameObject.Find("FirstPersonController").GetComponent<FirstPersonController>();
+        DD = FPC.diffcultyValue();
+        
+    }
+    private void Update()
+    {
+        //If candle is on and can be turn off, after correct amount of time turn off
+        if ((Time.time > time + timeToOff) && !unlit && canTurnOff)
+        {
+            //Debug.Log(unlit.ToString() + " "+ canTurnOff.ToString());
+            unlit = true;
+            flame.SetActive(false);
+            canTurnOff = false;
+        }
     }
     public override void OnFocus()
     {
@@ -23,10 +45,21 @@ public class LightCandles : Interactable
         //Interact with items
         if (unlit)
         {
-            print("Interacted with " + gameObject.name);
+            //Standard Turn on candle
             light.Play();
             flame.SetActive(true);
             unlit = false;
+            time = Time.time;
+
+            //Probabilty to turn off
+            float ran = Random.Range(0f, 100f);
+            Debug.Log(ran);
+            ran *= DD;
+            Debug.Log(DD);
+            if (ran >= threshold)
+            {
+                canTurnOff = true;
+            }
         }
         
     }
