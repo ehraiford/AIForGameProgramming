3a3172c6057aa9e893fd972e9620bf4b38902e10
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2022-11-02 12:20:53 -0400

    Added game over audio
    Added game over audio. Tried to get a slow fade for the text but couldn't get it to work.

--- Breakout From Ackers/Assets/Scripts/UI/GameOverScript.cs
+++ Breakout From Ackers/Assets/Scripts/UI/GameOverScript.cs
@@ -1,38 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameOverScript : MonoBehaviour
 {
     private float timePassed;
-    [SerializeField] GameObject firstText;
-    [SerializeField] GameObject secondText;
-    // Start is called before the first frame update
+    [SerializeField] TextMeshProUGUI firstText;
+    [SerializeField] TextMeshProUGUI secondText;
+    [SerializeField] AudioSource audioSource;
+    
+
+    //freezes time, starts the audio, and starts tracking time passed.
     private void Awake()
     {
         timePassed = Time.realtimeSinceStartup;
         Time.timeScale = 0.0f;
-        firstText.SetActive(false);
-        secondText.SetActive(false);
+
+        firstText.color = new Color(0, 0, 0, 255);
+        secondText.color = new Color(0, 0, 0, 255);
+
+
+        audioSource.Play();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Time.realtimeSinceStartup - timePassed > 5 && Time.realtimeSinceStartup - timePassed < 10)
+       
+        //switch case handles the opacity changes of the text and then restarts time and removes the death screen after 18 seconds
+        switch(Time.realtimeSinceStartup - timePassed)
         {
-            firstText.SetActive(true);
-        }
-        else if (Time.realtimeSinceStartup - timePassed >= 10 && Time.realtimeSinceStartup - timePassed < 15)
-        {
-            secondText.SetActive(true);
-        }
-        else if (Time.realtimeSinceStartup - timePassed >= 15)
-        {
-            Time.timeScale = 1.0f;
-            gameObject.SetActive(false);
+            case  <3:
+                break;
+            case <10:
+                ChangeAlpha(firstText, 0.002f);
+                break;
+            case <18:
+                ChangeAlpha(secondText, 0.002f);
+                break;
+            default:
+                Time.timeScale = 1.0f;
+                gameObject.SetActive(false);
 
-            //Logan, if you want to move the player character or reset their health or anything through the script, this would be the spot to do it.
+                //Logan, if you want to move the player character or reset their health or anything through the script, this would be the spot to do it.
+                break;
         }
+       
+    }
+
+    //Adds or Subtracts the alpha difference from the text.
+    private void ChangeAlpha(TextMeshProUGUI text, float alphaChange)
+    {
+        text.color = new Color(1, 1, 1, text.faceColor.a + alphaChange);
+        Debug.Log("New Alpha:" + text.color.a);
     }
 }
