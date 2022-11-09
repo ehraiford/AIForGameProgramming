d28fa20c5d72a6c878d19c1f1b6eb25a41e331ff
QuanminhN <quanminhn1@gmail.com>
2022-11-09 00:02:57 -0500

    LookAt Gunshot
    Started implementing enemy to look at gunshot

--- Breakout From Ackers/Assets/HeardGunshot.cs
+++ Breakout From Ackers/Assets/HeardGunshot.cs
@@ -3,53 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class IdleBehavior : StateMachineBehaviour
+public class HeardGunshot : StateMachineBehaviour
 {
-    float timer;
     NavMeshAgent agent;
     Transform player;
+    float rotateSpeed = 5f;
+    float time;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        timer = 0;
         agent = animator.GetComponentInParent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        time = Time.time;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        float distance = Vector3.Distance(animator.transform.position, player.position);
-        if (Input.GetButtonDown("Fire1") && distance < 10)
-        {
-            animator.SetTrigger("heardGunshot");
-        }
-        /*timer += Time.deltaTime;
-        if (timer > 5)
+
+        //Use current object Y so it doesnt rotate
+        Vector3 targetPos = new Vector3(player.position.x, animator.transform.position.y, player.position.z);
+        agent.transform.LookAt(targetPos * rotateSpeed);
+        if(time + 5 < Time.time)
         {
             animator.SetBool("isPatrolling", true);
+
         }
-        if (agent.GetComponent<FOV>().canSeePlayer)
+        if (animator.GetComponent<FOV>().canSeePlayer)
         {
             animator.SetBool("isChasing", true);
-        }*/
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
-    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        // Implement code that processes and affects root motion
-    }
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
 
     // OnStateIK is called right after Animator.OnAnimatorIK()
-    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        // Implement code that sets up animation IK (inverse kinematics)
-    }
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
 }
