9701cd48198e7874b9ee98e50f451f928b539d26
Logan Blair <loganb3142@gmail.com>
2022-11-14 01:09:41 -0500

    Finished MedKit Equip/Unequip and Holding Anims

--- Breakout From Ackers/Assets/Scripts/Items/Medkit.cs
+++ Breakout From Ackers/Assets/Scripts/Items/Medkit.cs
@@ -2,32 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Medkit : MonoBehaviour
+public class MedKit : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float healTime = 1f;
 
-    private GameObject fpsController;
+    [SerializeField] private FirstPersonController playerController;
 
-    // Start is called before the first frame update
     void Start()
     {
-        fpsController = GameObject.Find("FirstPersonController");
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //If you want a different input, change it here
+        // If you want a different input, change it here
         if (Input.GetButtonDown("Fire1"))
         {
             StartCoroutine(Heal());
         }
     }
 
-    IEnumerator Heal()
+    private IEnumerator Heal()
     {
         yield return new WaitForSeconds(healTime);
-        fpsController.GetComponent<FirstPersonController>().AddHealth(50);
+        playerController.AddHealth(50);
     }
 }
