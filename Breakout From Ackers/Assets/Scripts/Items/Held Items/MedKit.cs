972f3cbdb34e6a2470cc50be66f30549b764c142
Logan Blair <loganb3142@gmail.com>
2022-11-16 02:20:30 -0500

    Added pill anims

---
+++ Breakout From Ackers/Assets/Scripts/Items/Held Items/MedKit.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedKit : MonoBehaviour
+{
+    [Header("Settings")]
+    private float healTime = 4f;
+
+    [Header("Object References")]
+    [SerializeField] private FirstPersonController playerController;
+    [SerializeField] private GameObject itemHandler;
+    private Animator playerAnimator;
+    public bool isHealing = false;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource medKitAudioSource = default;
+    [SerializeField] private AudioClip zipper = default;
+    [SerializeField] private AudioClip bandage = default;
+
+    void Start()
+    {
+        playerAnimator = playerController.GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        // Heal
+        if (Input.GetButtonDown("Fire1") && !isHealing && !itemHandler.GetComponent<ItemSwitching>().isSwitching)
+        {
+            StartCoroutine(Heal());
+        }
+    }
+
+    private IEnumerator Heal()
+    {
+        isHealing = true;
+        playerAnimator.SetBool("Healing", true);
+
+        // Wait for hand to reach MedKit then play zipper sound effect
+        yield return new WaitForSeconds(0.5f);
+        medKitAudioSource.PlayOneShot(zipper);
+
+        // Wait for zipper sound effect to stop then play bandage sound effect
+        yield return new WaitForSeconds(0.5f);
+        medKitAudioSource.PlayOneShot(bandage);
+
+        // Heals the player once animation is done
+        yield return new WaitForSeconds(healTime - 2f);
+        playerController.AddHealth(50);
+
+        // Finds the MedKit position in the player's inventory
+        int medKitSlot = 0 ;
+        for(int i = 0; i < 8; i++)
+        {
+            if (playerController.inventoryItems[i] == "MedKit") medKitSlot = i;
+        }
+
+        // Removes a MedKit from the player's inventory
+        playerController.RemoveInventoryItem("MedKit", 1);
+
+        
+        // Checks if the player runs out of MedKits
+        if (playerController.inventoryItemsCount[medKitSlot] > 0) // Player has more MedKits
+        {
+            playerAnimator.SetBool("RemainingMedKit", true);
+            playerAnimator.SetBool("Healing", false);
+        }
+        else // Player runs out of MedKits
+        {
+            playerAnimator.SetBool("RemainingMedKit", false);
+            playerAnimator.SetBool("Healing", false);
+            playerController.GetComponentInChildren<ItemSwitching>().NoRemaingingItemsFindNext();
+        }
+        
+
+       
+
+        isHealing = false;
+    }
+}
---
+++ Breakout From Ackers/Assets/Scripts/Items/Held Items/Pills.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pills : MonoBehaviour
+{
+    [Header("Settings")]
+    private float popTime = 3f;
+
+    [Header("Object References")]
+    [SerializeField] private FirstPersonController playerController;
+    [SerializeField] private GameObject itemHandler;
+    private Animator playerAnimator;
+    public bool isPopping = false;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource pillsAudioSource = default;
+    [SerializeField] private AudioClip capPop = default;
+    [SerializeField] private AudioClip pillShake = default;
+    [SerializeField] private AudioClip swallow = default;
+
+    void Start()
+    {
+        playerAnimator = playerController.GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        // Heal
+        if (Input.GetButtonDown("Fire1") && !isPopping && !itemHandler.GetComponent<ItemSwitching>().isSwitching)
+        {
+            StartCoroutine(Pop());
+        }
+    }
+
+    private IEnumerator Pop()
+    {
+        isPopping = true;
+        playerAnimator.SetBool("Popping", true);
+
+        // Wait for hands to be in position then play cap pop sound
+        yield return new WaitForSeconds(0.5f);
+        pillsAudioSource.PlayOneShot(capPop);
+
+        // Wait for the cap pop to stop then play shake pill sound
+        yield return new WaitForSeconds(0.5f);
+        pillsAudioSource.PlayOneShot(pillShake);
+
+        // Wait for the pill shake to stop and hand to reach face then play swallow sound
+        yield return new WaitForSeconds(1f);
+        pillsAudioSource.PlayOneShot(swallow);
+
+        // Removes debuff the player once animation is done
+        yield return new WaitForSeconds(popTime - 2.75f);
+        playerController.undoDebuff();
+
+        // Finds the Blue Mass Pills position in the player's inventory
+        int pillsSlot = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            if (playerController.inventoryItems[i] == "Blue Mass Pills") pillsSlot = i;
+        }
+
+        // Removes a MedKit from the player's inventory
+        playerController.RemoveInventoryItem("Blue Mass Pills", 1);
+
+
+        // Checks if the player runs out of MedKits
+        if (playerController.inventoryItemsCount[pillsSlot] > 0) // Player has more MedKits
+        {
+            playerAnimator.SetBool("RemainingPills", true);
+            playerAnimator.SetBool("Popping", false);
+        }
+        else // Player runs out of MedKits
+        {
+            playerAnimator.SetBool("RemainingPills", false);
+            playerAnimator.SetBool("Popping", false);
+            playerController.GetComponentInChildren<ItemSwitching>().NoRemaingingItemsFindNext();
+        }
+
+        isPopping = false;
+    }
+}
