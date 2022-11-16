d615537584d0a1815ddb4a7802444d07bd554e7f
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2022-11-16 10:22:44 -0500

    Set up the room notification system.

--- Breakout From Ackers/Assets/Scripts/UI/passThroughDoorScript.cs
+++ Breakout From Ackers/Assets/Scripts/UI/passThroughDoorScript.cs
@@ -1,13 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class passThroughDoorScript : MonoBehaviour
 {
-   // private void OnCollisionEnter(Collider collision)
-    //{
-       // Vector3 point = collision.
-        //Debug.Log(point);
-     //   Debug.Log("Entered");
-    //}
+
+    public bool firstCollider, secondCollider;
+    [SerializeField] string firstRoomName, secondRoomName;
+    [SerializeField] GameObject roomInfo;
+    public int cameFromFirstSecondNone;
+    private void Update()
+    {
+        
+    }
+
+    internal void colliderEntered(bool isFirst)
+    {
+        if (isFirst)
+        {
+            firstCollider = true;
+        }
+        else
+        {
+            secondCollider = true;
+        }
+
+        if(cameFromFirstSecondNone == 0)
+        {
+            if (isFirst) cameFromFirstSecondNone = 1;
+            else cameFromFirstSecondNone = 2;
+        }
+    }
+
+    internal void colliderExited(bool isFirst)
+    {
+        if (isFirst)
+        {
+            firstCollider = false;
+        }
+        else
+        {
+            secondCollider = false;
+        }
+
+        if (firstCollider == false && secondCollider == false)
+        {
+            if (isFirst && cameFromFirstSecondNone == 2) roomInfo.GetComponent<roomInfoScript>().setRoomInfoText(firstRoomName);
+            else if (!isFirst && cameFromFirstSecondNone == 1) roomInfo.GetComponent<roomInfoScript>().setRoomInfoText(secondRoomName);
+            cameFromFirstSecondNone = 0;
+        }
+    }
 }
