4539dcfb49524d1f6f8cc72ea4a4d7e5a5dba09b
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2022-11-20 19:46:15 -0500

    Updated Candle Puzzle

--- Breakout From Ackers/Assets/Scripts/Items/sittingRoomCandlePuzzleScript.cs
+++ Breakout From Ackers/Assets/Scripts/Items/sittingRoomCandlePuzzleScript.cs
@@ -7,9 +7,10 @@ public class sittingRoomCandlePuzzleScript : MonoBehaviour
 {
 
     private float startTime;
-    private bool wrongCode = false, playedAudio = false;
+    private bool wrongCode = false, playedAudio = false, complete = false;
     [SerializeField] GameObject[] candles;
     [SerializeField] GameObject diningRoomDoor;
+    [SerializeField] AudioClip[] audioClips;
     void Start()
     {
         
@@ -22,32 +23,37 @@ public class sittingRoomCandlePuzzleScript : MonoBehaviour
         {
             wrongCode = true;
             startTime = Time.time;
-            Debug.Log(startTime);
         }
-        else if (AllLit())
+        else if (AllLit() && !complete)
         {
+            gameObject.GetComponent<AudioSource>().clip = audioClips[1];
+            gameObject.GetComponent<AudioSource>().Play();
             diningRoomDoor.GetComponent<Door>().isLocked = false; //unlock door to foyer
             diningRoomDoor.GetComponent<Door>().door.Play("Door2_Open"); //open door to foyer
             diningRoomDoor.GetComponent<Door>().isOpen = true;
-            Destroy(gameObject);
+            startTime = Time.time;
+            complete = true;
         }
 
         //this chunk of code handles blowing out the candles.
-        if(wrongCode && Time.time - startTime > 5)
+        if (wrongCode && Time.time - startTime > 4)
         {
-            for(int i = 0; i < candles.Length; i++)
+            for (int i = 0; i < candles.Length; i++)
             {
                 candles[i].GetComponent<LightCandles>().forceLightingState(false);
             }
             wrongCode = false;
             playedAudio = false;
             //calls the blow sound .2 seconds before they blow out.
-        } else if(wrongCode && !playedAudio && Time.time - startTime > 4.5)
+        }
+        else if (wrongCode && !playedAudio && Time.time - startTime > 3.5)
         {
             playedAudio = true;
+            gameObject.GetComponent<AudioSource>().clip = audioClips[0];
             gameObject.GetComponent<AudioSource>().Play();
         }
-        
+        if (!diningRoomDoor.GetComponent<Door>().isLocked && Time.time - startTime > 3) Destroy(gameObject);
+
     }
 
     private bool AllLit()
