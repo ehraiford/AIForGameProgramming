7658c907f1b77e4c2a4da6a80501af0146072f79
QuanminhN <quanminhn1@gmail.com>
2022-12-04 21:05:34 -0500

    Boss noises
    Boss has a sound to indicate that he is in "rage" mode and has subtle attack sound

--- Breakout From Ackers/Assets/Scripts/Zombie/BossStat.cs
+++ Breakout From Ackers/Assets/Scripts/Zombie/BossStat.cs
@@ -15,6 +15,8 @@ public class BossStat : CharacterStats
     public bool isDead;
     public Collider[] cols;
     NavMeshAgent agent;
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,6 +41,9 @@ public class BossStat : CharacterStats
         agent.speed = 4;
         attackSpeed = 1f;
         damage = 60f;
+        //Play sound to show the boss is angry
+        audioSource.PlayOneShot(audioClips[0]);
+
     }
     protected override void ApplyDamage(float dmg)
     {
